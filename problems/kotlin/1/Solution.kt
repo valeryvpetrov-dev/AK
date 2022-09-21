7152f3d3f2ab0ca043843ed2d4fe2889c2ac6b9d
Valery Petrov <valeryvpetrov.dev@gmail.com>
2022-09-21 20:15:43 +0300

    Add problem 1 Kotlin solution

---
+++ problems/kotlin/1/Solution.kt
@@ -0,0 +1,13 @@
+package problems.kotlin.1
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val numsToIdx = HashMap<Int, Int>()
+        for ((idx, num) in nums.withIndex()) {
+            val complement = target - num
+            numsToIdx[complement]?.let { return intArrayOf(it, idx) }
+            numsToIdx[num] = idx
+        }
+        return intArrayOf()
+    }
+}
