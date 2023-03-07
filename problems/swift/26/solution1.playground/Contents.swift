15b102922e2f4e34e0449ea4b5828436538f4bcf
Алекбаев Азат <aalekbaev@it-one.ru>
2023-03-07 12:51:10 +0500

    Add solution 26

---
+++ problems/swift/26/solution1.playground/Contents.swift
@@ -0,0 +1,29 @@
+class Solution {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+            var itemCount = 0
+    var prevItem: Int? = -100000
+    for (index, item) in nums.enumerated() {
+        guard index != nums.count - 1 else {
+            nums.removeAll(where: { $0 == -100000 })
+            return nums.count
+        }
+        
+        if item == nums[index + 1] || item == -100000 {
+            itemCount += 1
+            prevItem = item
+            if prevItem == nums[index + 1] {
+                nums[index + 1] = -100000
+            }
+        } else {
+            let element = nums[index + 1]
+            nums[index + 1 - itemCount] = element
+            prevItem = item
+            if itemCount > 0 {
+                nums[index + 1] = -100000
+            }
+        }
+    }
+    nums.removeAll(where: { $0 == -100000 })
+    return nums.filter { $0 != -100000 }.count
+    }
+}
